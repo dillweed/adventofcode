107f2628fc65c096f7dda4d14dc718e4594b8eb4
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-07 21:30:31 -0800

    03a completed

---
+++ 2022/03_commented_with_ChatGPT.py
@@ -0,0 +1,48 @@
+import re
+
+# Read the input file and store its lines in a list
+with open("03_input.txt") as file:
+    input_list = file.readlines()
+
+
+def replace_letters_with_numbers(text):
+    # Define the pattern to match letters
+    pattern = r"[a-zA-Z]"
+
+    # Replace the matched letters with their corresponding numbers
+    return re.sub(
+        pattern,
+        lambda m: str(
+            ord(m.group(0).lower()) - ord("a") + 1 + (26 if m.group(0).isupper() else 0)
+        ),
+        text,
+    )
+
+
+# Create an empty list to store unique items
+unique_items = []
+
+# Initialize the priority sum
+priority_sum = 0
+
+# Iterate over the lines in the input list
+for line in input_list:
+    # Find the middle index of the line
+    middle = len(line) // 2
+
+    # Initialize a flag to track if the current character has been found in the second half of the line
+    found = False
+
+    # Iterate over the first half of the line
+    for _c in line[:middle]:
+        if found:
+            break
+
+        # Check if the current character is in the second half of the line
+        if _c in line[middle:]:
+            found = True
+            # If it is, add its corresponding number to the priority sum
+            priority_sum += int(replace_letters_with_numbers(_c))
+
+# Print the final priority sum
+print(priority_sum)
--- 2022/03_rucksack_reorganization.py
+++ 2022/03_rucksack_reorganization.py
@@ -1,12 +1,31 @@
+import re
+
 with open("03_input.txt") as file:
     input_list = file.readlines()
 
-# Lowercase item types a through z have priorities 1 through 26.
-# Uppercase item types A through Z have priorities 27 through 52.
+
+def replace_letters_with_numbers(text):
+    pattern = r"[a-zA-Z]"
+    return re.sub(
+        pattern,
+        lambda m: str(
+            ord(m.group(0).lower()) - ord("a") + 1 + (26 if m.group(0).isupper() else 0)
+        ),
+        text,
+    )
+
 
 unique_items = []
-priority_dict = 
+priority_sum = 0
 
 for line in input_list:
-    for _c in line[:len(line)/2]:
-        if _c in line[len(line)/2:]:
+    middle = len(line) // 2
+    found = False
+    for _c in line[:middle]:
+        if found:
+            break
+        if _c in line[middle:]:
+            found = True
+            priority_sum += int(replace_letters_with_numbers(_c))
+
+print(priority_sum)
