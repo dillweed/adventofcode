237c9139abbc0e3e90594db390c93aebe48de81a
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-09 15:20:16 -0800

    Formatted stacks and procedures

--- 2022/05a_supply_stacks.py
+++ 2022/05a_supply_stacks.py
@@ -1,16 +1,18 @@
-# Read the input file and store its lines in a list
-with open("05_input.txt") as file:
-    input_list = file.readlines()
+# Read the input procedures file and store its lines in a list
+with open("05_input_test.txt") as file:
+    input_procedures = file.readlines()
 
 # Read the input stacks file and store as a string
-with open("05_input_stacks.txt") as file:
+with open("05_input_stacks_test.txt") as file:
     vertical_stacks = file.read()
 
 
 def main():
     print(vertical_stacks)
-    stacks = transpose_stacks(vertical_stacks)
+    stacks: dict[str, list[str]] = transpose_stacks(vertical_stacks)
     print(stacks)
+    procedures: list[list[int]] = parse_procedures(input_procedures)
+    print(procedures)
 
 
 def transpose_stacks(vertical_stacks):
@@ -31,6 +33,13 @@ def transpose_stacks(vertical_stacks):
                 hor_stack_indexes[i] = c  # Track digit position in layer
 
     return horizontal_stacks
+
+
+def parse_procedures(input_procedures: list[str]) -> list[list[int]]:
+    procedures = []
+    for line in input_procedures:
+        procedures.append([int(s) for s in line.split() if s.isdigit()])
+    return procedures
 
 
 if __name__ == "__main__":
