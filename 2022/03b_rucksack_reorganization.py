72c0a4dc46b507c7619aff544403cec1af5237e6
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-08 17:59:00 -0800

    03b complete

--- 2022/03b_rucksack_reorganization.py
+++ 2022/03b_rucksack_reorganization.py
@@ -22,24 +22,20 @@ def replace_letters_with_numbers(text):
 # Initialize the priority sum
 priority_sum = 0
 
-# Iterate over the lines in the input list
-for line in input_list:
-    # Find the middle index of the line
-    middle = len(line) // 2
-
-    # Initialize a flag to track if the current character has been found in the second half of the line
+for group in zip(input_list[::3], input_list[1::3], input_list[2::3]):
     found = False
-
-    # Iterate over the first half of the line
-    for _c in line[:middle]:
+    group = list(group)
+    for i, item in enumerate(group):
+        group[i] = item.strip()
+    print(group)
+    for _c in group[0]:
         if found:
             break
-
-        # Check if the current character is in the second half of the line
-        if _c in line[middle:]:
-            found = True
-            # If it is, add its corresponding number to the priority sum
-            priority_sum += int(replace_letters_with_numbers(_c))
+        else:
+            if _c in group[1] and _c in group[2]:
+                print(_c)
+                found = True
+                priority_sum += int(replace_letters_with_numbers(_c))
 
 # Print the final priority sum
 print(priority_sum)
