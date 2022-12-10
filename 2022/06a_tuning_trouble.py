d89cf2194c6fe1eed719f861c85cdd4d6f18e559
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-09 18:01:29 -0800

    Completed 06a and 06b

---
+++ 2022/06a_tuning_trouble.py
@@ -0,0 +1,16 @@
+# Read the input datastream file and store as a string
+with open("06_input.txt") as file:
+    signal = file.read()
+
+set_min = 4
+# Init flag to break loop if found
+found = False
+# loop through the characters in the signal string
+for i, c in enumerate(signal):
+    if found:
+        break
+    # check if length of set of substring is set_min
+    if len(set(signal[i : i + set_min])) == set_min:
+        print(set(signal[i : i + set_min]))
+        print(i + set_min)
+        found = True
