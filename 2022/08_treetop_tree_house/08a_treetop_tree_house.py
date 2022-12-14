0b79baa6ee3ace077f7919a5e410617f42c8ef71
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-13 17:58:18 -0800

    08a complete

--- 2022/08_treetop_tree_house/08a_treetop_tree_house.py
+++ 2022/08_treetop_tree_house/08a_treetop_tree_house.py
@@ -4,13 +4,48 @@
 def main():
     """TBA."""
     # Define input filename
-    input_file = "08_input_test.txt"
+    input_file = "08_input.txt"
     # Load input string as 2d array
     trees = load_input_file(input_file)
-
     [print(*row) for row in trees]
-    print("row2 reversed:", trees[1][::-1])
-    print("col2 reversed:", [row[1] for row in trees][::-1])
+    hidden = num_hidden(trees)
+    # I accidentally wrote this for hidden trees, not visible.
+    total_trees = sum([len(row) for row in trees])
+    print("Number of visible trees:", total_trees - hidden)
+
+
+def num_hidden(trees) -> int:
+    hidden = 0
+    tree_dict = {}
+
+    # Check horizontal tree visibility
+    for i, row in enumerate(trees):
+        if i == 0 or i == len(trees) - 1:  # Skip edge rows
+            print("edge row:", row)
+            continue
+        for j in range(1, len(row) - 1):
+            print("row:", row, ", item:", row[j], ", i:", i, ", j:", j)
+            # Assign boolean result in list as value to i,j coordinate key
+            tree_dict[i, j] = [row[j] <= max(row[:j]) and row[j] <= max(row[j + 1 :])]
+            print(row[j], row, tree_dict[i, j])
+
+    # Check vertical tree visibility
+    for col_index in range(1, len(trees[0]) - 1):  # Skip edge columns
+        col = []  # Individual column
+        for i, row in enumerate(trees):
+            col.append(row[col_index])
+        print(col)
+        for i in range(1, len(col) - 1):  # Skip column ends
+            # Append boolean result to list value of i,col_index coordinate key
+            tree_dict[i, col_index].append(
+                col[i] <= max(col[:i]) and col[i] <= max(col[i + 1 :])
+            )
+            print("col i:", col[i], tree_dict[i, col_index])
+
+    print(tree_dict)
+    # Count "True, True" pairs from coordinate dict
+    hidden = sum([i and j for i, j in tree_dict.values()])
+    return hidden
 
 
 def load_input_file(input_file) -> list[list[int]]:
