264133f0b722ffed27a23f86ee15f565cc8491e1
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-15 23:13:23 -0800

    stay on target

--- 2022/08_treetop_tree_house/08a_treetop_tree_house_redo.py
+++ 2022/08_treetop_tree_house/08a_treetop_tree_house_redo.py
@@ -8,12 +8,17 @@ def main():
 
     input_file = "08_input_test.txt"
     forest = load_forest(input_file)
-    pprint(forest)
+    max_x = max([tree._x for tree in forest.values()])
+    max_y = max([tree._y for tree in forest.values()])
+
+    print("Visible trees:", sum([tree.visible for tree in forest.values()]))
+
+    # if forest[3, 2]:
+    #     print(max_x, max_y)
 
     # # Change attributes after instantiation
-    # forest[3, 2].things.append("aoeu")
-    # forest[3, 2].things.append(234)
-    # forest[3, 2].set_visible()
+    # forest[3, 2].view.append("aoeu")
+    # forest[3, 2].view.append(234)
     # print("Updated tree:", forest[3, 2])
 
     # print(
@@ -24,10 +29,10 @@ def main():
     # a_tree = forest[4, 2]
     # x = -2
     # y = 1
-    # print(f"tree height {a_tree.height} at {a_tree.x_i}, {a_tree.y_i} coordinates")
+    # print(f"tree height {a_tree.height} at {a_tree._x}, {a_tree._y} coordinates")
     # print(
     #     f"Compare trees using relative {x}, {y} coordinates:",
-    #     forest[a_tree.x_i + x, a_tree.y_i + y].height,
+    #     forest[a_tree._x + x, a_tree._y + y].height,
     # )
     # print("Out of forest coordinate error:", forest[5, 6])
 
@@ -42,14 +47,15 @@ def load_forest(input_file) -> dict:
     return forest
 
 
-@dataclass(frozen=True, order=True)
+@dataclass(frozen=True)
 class Tree:
-    x_i: int
-    y_i: int
+    _x: int
+    _y: int
     height: int
     list_i: int
-    things: list[int] = field(default_factory=list)
-    visible: bool = field(default=False)
+    edge: bool = False
+    view: list[int] = field(default_factory=list)
+    visible: bool = False
 
     def set_visible(self):
         object.__setattr__(self, "visible", True)
