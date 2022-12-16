4a2380c7aaf4c79fd9dac464f1aa5dc69385c5f3
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-15 22:40:16 -0800

    I discovered dataclasses

---
+++ 2022/08_treetop_tree_house/08a_treetop_tree_house_redo.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass, asdict, astuple, field
+from pprint import pprint
+from collections import defaultdict
+
+
+def main():
+    """TBA."""
+
+    input_file = "08_input_test.txt"
+    forest = load_forest(input_file)
+    pprint(forest)
+
+    # # Change attributes after instantiation
+    # forest[3, 2].things.append("aoeu")
+    # forest[3, 2].things.append(234)
+    # forest[3, 2].set_visible()
+    # print("Updated tree:", forest[3, 2])
+
+    # print(
+    #     "Compare heights using absolute coordinates",
+    #     forest[1, 1].height.__ge__(forest[2, 4].height),
+    # )
+
+    # a_tree = forest[4, 2]
+    # x = -2
+    # y = 1
+    # print(f"tree height {a_tree.height} at {a_tree.x_i}, {a_tree.y_i} coordinates")
+    # print(
+    #     f"Compare trees using relative {x}, {y} coordinates:",
+    #     forest[a_tree.x_i + x, a_tree.y_i + y].height,
+    # )
+    # print("Out of forest coordinate error:", forest[5, 6])
+
+
+def load_forest(input_file) -> dict:
+    trees_list = load_input_file(input_file)
+    forest: dict = defaultdict(lambda: None)
+    for y, line in enumerate(trees_list):
+        for x, height in enumerate(line):
+            this_tree = Tree(x, y, height, list_i=len(forest))
+            forest[x, y] = this_tree
+    return forest
+
+
+@dataclass(frozen=True, order=True)
+class Tree:
+    x_i: int
+    y_i: int
+    height: int
+    list_i: int
+    things: list[int] = field(default_factory=list)
+    visible: bool = field(default=False)
+
+    def set_visible(self):
+        object.__setattr__(self, "visible", True)
+
+
+def load_input_file(input_file) -> list[list[int]]:
+    with open(input_file) as file:
+        trees_str = file.read()
+
+    trees_list: list[list] = [[] for _ in range(len(trees_str.split("\n")))]
+
+    for i, line in enumerate(trees_str.split("\n")):
+        for j, c in enumerate(line):
+            trees_list[i].append(int(c))
+
+    return trees_list
+
+
+if __name__ == "__main__":
+    main()
