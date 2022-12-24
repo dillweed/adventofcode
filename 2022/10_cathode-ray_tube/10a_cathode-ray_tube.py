bd955052901ff97dedb772540297d436bf3f0870
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-24 13:34:49 -0800

    Added cycles_with_addx to help me understand this.

--- 2022/10_cathode-ray_tube/10a_cathode-ray_tube.py
+++ 2022/10_cathode-ray_tube/10a_cathode-ray_tube.py
@@ -1,18 +1,20 @@
 from pprint import pprint
+
 """Process signal input to find answer to 10a."""
 
 
 def main():
     """TBA."""
     # Define input filename
-    input_file = "10_input.txt"
+    input_file = "10_input_test.txt"
     # Load input string as list
     signal = load_input_file(input_file)
     # [print(line) for line in signal]
-    cycles = process_signal(signal)
-    pprint(cycles)
-    print(cycles[220])
-    pprint(list((i, cycle) for i, cycle in enumerate(cycles)))
+    cycles, cycles_with_addx = process_signal(signal)
+    # pprint(cycles)
+    # print(cycles[220])
+    # pprint(list((i, cycle) for i, cycle in enumerate(cycles)))
+    pprint(list((i, cycle) for i, cycle in enumerate(cycles_with_addx)))
     start = 20
     interval = 40
     strengths = query_cycles(cycles, start, interval)
@@ -22,26 +24,34 @@ def main():
 
 def process_signal(signal):
     cycles = [1]
+    # TODO add another list for comparison to addx values
+    cycles_with_addx = [0]
     target_cycle = 0
     pending = 0
     for item in signal:
         current_cycle = len(cycles)
         if current_cycle == target_cycle:
             cycles.append(cycles[-1] + pending)
+            cycles_with_addx.append(
+                ("n:", (cycles[-1]), "added", pending, "next", item)
+            )
         else:
             cycles.append(cycles[-1])
+            cycles_with_addx.append(("n:", (cycles[-1]), "prev noop", item))
+
         if item != None:  # Found an int
             cycles.append(cycles[-1])
+            cycles_with_addx.append(("n:", (cycles[-1]), "pending", item))
             target_cycle = len(cycles)
             pending = item
 
-    return cycles
+    return cycles, cycles_with_addx
 
 
 def query_cycles(cycles, start, interval):
     strengths = []
     for i in range(start, len(cycles), interval):
-        strengths.append(cycles[i]*i)
+        strengths.append(cycles[i] * i)
     return strengths
 
 
@@ -49,7 +59,7 @@ def load_input_file(input_file) -> list:
     signal = []
     with open(input_file) as file:
         for line in file.readlines():
-            if line.strip() != 'noop':
+            if line.strip() != "noop":
                 # Throw away "addx" and keep int
                 signal.append(int(line.split()[1]))
             else:
